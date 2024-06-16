a22bc8d9d2d1ef4097ce51c042fabcfe5d98432f
imrankhan992 <imrankohistani65@gmail.com>
2024-06-16 23:17:34 +0500

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ButtonGradient from "./assets/svg/ButtonGradient";
+import Button from "./components/Button";
+import Header from "./components/Header";
+const App = () => {
+  return (
+    <>
+      {/* <h1 className="text-3xl font-bold underline">Hello world!</h1> */}
+      <div className="">
+        <Header />
+      </div>
+      {/* <ButtonGradient /> */}
+    </>
+  );
+};
+
+export default App;
